eb0db6f48205612c413bf84c9c0788822b949dbb
Sam Rice <samsrice@gmail.com>
2022-12-28 18:11:40 -0800

    reorganize action folder

---
+++ src/actions/CollectionActions.js
@@ -0,0 +1,29 @@
+export const createCollection = name => ({
+  type: "ADD_COLLECTION",
+  payload: {
+    name,
+    id: Date.now()
+  }
+})
+
+export const deleteCollection = id => ({
+  type: "DELETE_COLLECTION",
+  payload: { id }
+})
+
+export const updateNote = (text, collectionID, objectID) => ({
+  type: "UPDATE_NOTE",
+  payload: {
+    text,
+    collectionID,
+    objectID
+  }
+})
+
+export const deletePiece = (collectionID, objectID) => ({
+  type: "DELETE_PIECE",
+  payload: {
+    collectionID,
+    objectID
+  }
+})

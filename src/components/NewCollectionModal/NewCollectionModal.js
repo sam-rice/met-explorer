4926130f147ea31dfb375b4db95a47dc38d3a546
Sam Rice <samsrice@gmail.com>
2023-01-05 11:37:19 -0800

    add additional testing for modal to CollectionsList spec

--- src/components/NewCollectionModal/NewCollectionModal.js
+++ src/components/NewCollectionModal/NewCollectionModal.js
@@ -11,11 +11,6 @@ const NewCollectionModal = ({ isOpen, collections, submitCollection, closeModal 
 
   ReactModal.setAppElement("#root")
 
-  // put in use effect?
-  // setNameExistsError(false)
-  // setFormError(false)
-  // setName("")
-
   const handleKeyDown = e => {
     if (e.key === "Enter") handleSubmit()
   }
@@ -33,52 +28,56 @@ const NewCollectionModal = ({ isOpen, collections, submitCollection, closeModal 
       return
     }
     submitCollection(name)
-    // dispatch(createCollection(name))
-    // setModalOpen(false)
-    // setName("")
+  }
+
+  const handleClose = () => {
+    setNameExistsError(false)
+    setFormError(false)
+    setName("")
+    closeModal()
   }
 
   return (
     <Modal
-        className="modal"
-        isOpen={isOpen}
-        closeTimeoutMS={400}
-        contentLabel="modal"
-        autoFocus={false}
-      >
-        <h3>create new collection:</h3>
-        <div className="modal__form">
-          <label
-            htmlFor="collection-name"
-          >
-            name:
-          </label>
-          <input
-            className="modal__form__input"
-            id="collection-name"
-            name="collection name"
-            value={name}
-            onChange={e => setName(e.target.value)}
-            onKeyDown={e => handleKeyDown(e)}
-            autoFocus={true}
-            data-cy="modal-input"
-          />
-          <button
-            className="modal__form__button"
-            onClick={handleSubmit}
-            data-cy="modal-submit"
-          >create
-          </button>
-          {formError && <p className="modal__form__error">*required field</p>}
-          {nameExistsError && <p className="modal__form__error">*collection name already exists</p>}
-        </div>
+      className="modal"
+      isOpen={isOpen}
+      closeTimeoutMS={400}
+      contentLabel="modal"
+      autoFocus={false}
+    >
+      <h3>create new collection:</h3>
+      <div className="modal__form">
+        <label
+          htmlFor="collection-name"
+        >
+          name:
+        </label>
+        <input
+          className="modal__form__input"
+          id="collection-name"
+          name="collection name"
+          value={name}
+          onChange={e => setName(e.target.value)}
+          onKeyDown={e => handleKeyDown(e)}
+          autoFocus={true}
+          data-cy="modal-input"
+        />
         <button
-          className="modal__close-button"
-          onClick={closeModal}
-          data-cy="modal-close"
-        >cancel
+          className="modal__form__button"
+          onClick={handleSubmit}
+          data-cy="modal-submit"
+        >create
         </button>
-      </Modal>
+        {formError && <p className="modal__form__error" data-cy="required-error">*required field</p>}
+        {nameExistsError && <p className="modal__form__error" data-cy="existing-name-error">*collection name already exists</p>}
+      </div>
+      <button
+        className="modal__close-button"
+        onClick={handleClose}
+        data-cy="modal-close"
+      >cancel
+      </button>
+    </Modal>
   )
 }
 
